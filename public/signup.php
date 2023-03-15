57058e40b58e859c1ed509fe68369ee8a1e12e2f
BoyBroccoli <boybroccoli@pm.me>
2023-03-15 00:17:34 -0300

    signup complete, must link to db

--- public/signup.php
+++ public/signup.php
@@ -8,29 +8,35 @@ include_once("../src/libs/helpers.php");
         
         <div class="container">
             <h1 class="text-center">Sign Up Form</h1>
-            <div class="mb-3">
-                <label for="userName">Username:</label>
-                <input type="text" class="form-control" name="userName" id="userName" placeholder="Input desired username">
+            <div class="row mb-3">
+                <div class="col-6">
+                    <label for="userName" class="form-label">Username:</label>
+                    <input type="text" class="form-control" name="userName" id="userName" placeholder="Input desired username">
+                </div>
+                <div class="col">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" class="form-control" name="email" id="email" placeholder="email@example.com">
+                </div>
             </div>
 
             <div class="row mb-3">
-                <div class="col">
-                    <label for="fName">First Name:</label>
+                <div class="col-6">
+                    <label for="fName" class="form-label">First Name:</label>
                     <input type="text" class="form-control" name="fName" id="fName" placeholder="Enter First Name">
                 </div>
                 <div class="col">
-                    <label for="lName">Last Name:</label>
+                    <label for="lName" class="form-label">Last Name:</label>
                  <input type="text" class="form-control" name="lName" id="lName" placeholder="Enter Last Name">
                 </div>
             </div>
 
             <div class="row mb-3">
-                <div class="col">
-                    <label for="password">Password:</label>
+                <div class="col-6">
+                    <label for="password" class="form-label">Password:</label>
                  <input type="password" class="form-control" name="password" id="password" placeholder="Enter password">
                 </div>
                 <div class="col">
-                    <label for="password2">Password Again:</label>
+                    <label for="password2" class="form-label">Password Again:</label>
                     <input type="password" class="form-control" name="password2" id="password2" placeholder="Enter exact password again">
                 </div>
                 
