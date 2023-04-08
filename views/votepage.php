48aa5571c8614d17d79ff20b78fc068f6f742528
Dawan Savage Bell <savagebell00@gmail.com>
2023-04-08 16:07:03 -0300

    showing radio buttons and candidate options

--- views/votepage.php
+++ views/votepage.php
@@ -36,21 +36,115 @@
 <body>    
     <?php if (isset($user)) : ?>
 
-        <div class="container">
-            <h1>Hello <?= htmlspecialchars($user["fName"]) ?>, Vote Today!</h1>
-            <h2>Candidates!</h2>
-            <row>
-                <?php
-                    SELECT_EVERYTHING_FROM_CANDIDATE($conn);
-                ?>
-            </row>
-        </div>
+        <h1>Hello <?= htmlspecialchars($user["fName"]) ?>, Vote Today!</h1>
+            <!-- container for displaying candidate table and data results -->
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <!-- card header -->
+                        <div class="card-header">
+                            <h3>
+                                Current Candidates!
+                                <!-- button for voting for a candidate -->
+                                <button type="button" id="voteTodayBtn"class="btn btn-primary float-end"
+                                    data-bs-toggle="modal" data-bs-target="#voteForCandidateModal">
+                                    Vote Today!
+                                </button>
+                            </h3>
+                        </div>
 
-        <p><a href="./logout.php">Log out</a></p>
+                        <!-- card body to fetch data from candidate table -->
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="candidateTable" class="table table-bordered table-striped table-hover">
+                                    <!-- table head -->
+                                    <thead>
+                                        <!-- table row -->
+                                        <tr>
+                                            <!-- table column headings -->
+                                            <th id="candidateName">Candidate Name</th>
+                                            <th id="candidateSlogan">Candidate Slogan</th>
+                                        </tr>
+                                    </thead>
+                                    <!-- table body -->
+                                    <tbody>
+                                        <?php
+                                            $conn = CONNECT_MYSQL();
+                                            SELECT_EVERYTHING_FROM_CANDIDATE($conn);
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- container for holding radio buttons -->
+            <div class="container">
+                <h2>Choose Your Candidate</h2>
+                <div class="row">
+                    <form id="candidateForm" action="../models/voteForCandidate.php" >
+                        <!-- radio buttons for candidates -->
+                        <input type="radio" id="candidateChoice1"  name="candidateChoices" value="Boy Broccoli">
+                        <label for="boyBroc">Boy Broccoli</label><br>
+
+                        <input type="radio" id="candidateChoice2"  name="candidateChoices" value="Barrack Obama">
+                        <label for="barackObama">Barrack Obama</label><br>
+
+                        <input type="radio" id="candidateChoice3"  name="candidateChoices" value="Mo Kamalian">
+                        <label for="moKamalian">Mo Kamalian</label><br>
+
+                        <input type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
 
     <?php else: ?>
         <p><a href="./login.php">Log in</a> or <a href="./signup.php">Sign up</a></p>
-    
     <?php endif; ?>
+
 </body>
+
+    <div class="footer">
+        <p><a href="./logout.php">Log out</a></p>
+    </div>
 </html>
+
+    <!-- jquery script -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <!-- JavaScript for alertify-->
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+    <!-- Custom ajax script event listener -->
+    <script>
+
+        var user_id = $(this).val();
+
+        // for update users candidate vote on form voteForCandidateForm
+        $(document).on('click', '.voteTodayBtn', function () {
+            $.ajax({
+                type: "GET",
+                url: "models/voteForCandidateModal.php?" + user_id,
+                success: function(response) {
+
+                    var res= $.parseJSON(response);
+
+                    if (res.status == 422) { // if input errors
+
+                        alert.console.error(res.message);
+
+                    } else if (res.status == 200) { // when succesful
+                        
+                        // storing data fetched from response
+                    }
+                }
+            });
+            
+            
+        });
+
+
+
+    </script>
